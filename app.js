70083548ed2e3dd5375cfe5772ce6ce70c46fa39
emzhran <zachran750@gmail.com>
2024-10-25 19:43:50 +0700

    CRUD_RESTAPI

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const todosRoutes = require('./routes/data');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.use('/todos', todosRoutes);
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
